a94afc7d4b5f7c6a251d89aeadb1cbed6e6d9e54
Mike <382847508@qq.com>
2019-02-13 14:50:44 +0800

    数据上传

--- tutorial/download/dbutil.py
+++ tutorial/download/dbutil.py
@@ -4,11 +4,11 @@ import MySQLdb
 from porn import Porn
 
 class DButils():
-    db = None
+    db=None
     _cursor = None
     def __init__(self):
-        db = MySQLdb.connect(host='127.0.0.1', user='root', passwd='x5', db='mrporn', charset='utf8')
-        self._cursor = db.cursor()
+        self.db = MySQLdb.connect(host='127.0.0.1', user='root', passwd='x5', db='mrporn', charset='utf8')
+        self._cursor = self.db.cursor()
 
     def get_non_download(self):
         sql = "SELECT PORNID,PORNNAME,PORNURL,PORNCONTENT,PORNPAGE,PORNDOWNLOAD FROM PORNS WHERE PORNDOWNLOAD = 'F' ORDER BY PORNID LIMIT 1"
@@ -20,8 +20,15 @@ class DButils():
     def update_get_pre_download(self,pornid):
         sql = "UPDATE PORNS SET PORNDOWNLOAD = 'R' WHERE PORNDOWNLOAD = 'F' AND PORNID = "+str(pornid)
         self._cursor.execute(sql)
+        self.db.commit()
         sql = "SELECT PORNID,PORNNAME,PORNURL,PORNCONTENT,PORNPAGE,PORNDOWNLOAD FROM PORNS WHERE PORNDOWNLOAD = 'R' AND PORNID = "+str(pornid)
         self._cursor.execute(sql)
         result = self._cursor.fetchone()
         porn = Porn(result[0], result[1], result[2], result[3], result[4], result[5])
         return porn
+
+    def update_finish_download(self,pornid):
+        sql = "UPDATE PORNS SET PORNDOWNLOAD = 'T',DOWNTIME=NOW() WHERE PORNDOWNLOAD = 'R' AND PORNID = " + str(pornid)
+        self._cursor.execute(sql)
+        self.db.commit()
+        self.db.close()
--- tutorial/download/execute.py
+++ tutorial/download/execute.py
@@ -6,8 +6,7 @@ from downloadbatch import GetFile
 
 if __name__ == '__main__':
     ADDRESS = "E:\\Users\\tutorial\\tutorial\\movies\\"
-    db = DButils()
-    nonporn = db.get_non_download()
+    nonporn = DButils().get_non_download()
     reg = "(http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?"
     while not re.match(reg, nonporn.porncontent):
         print("网址格式错误")
@@ -17,8 +16,8 @@ if __name__ == '__main__':
     filename = file1.getfilename()
     print ("下载的文件为：" + str('%.2f' % (file_total / 1024 / 1024)) + "MB")
     print ("开始下载文件:" + str(filename))
-    preporn = db.update_get_pre_download(nonporn.pornid)
-    t1 = threading.Thread(target=file1.downfile, args=(str(ADDRESS+filename),))
+    preporn = DButils().update_get_pre_download(nonporn.pornid)
+    t1 = threading.Thread(target=file1.downfile, args=(str(ADDRESS+filename),preporn.pornid))
     t1.start()
 
 
